59ab0532e30b856a7ec47cad6832026624bb0c8b
RathodKunal11 <rathodvkunal11@gmail.com>
2023-03-04 18:19:43 +0530

    Commit for Master Branch

---
+++ src/GitDemo/PracticeMergeConflict.java
@@ -0,0 +1,22 @@
+package GitDemo;
+
+public class PracticeMergeConflict {
+	
+	public void m1() {
+		System.out.println("This is method m1");
+	}
+	
+	public void m2() {
+		System.out.println("This is method m2");
+	}
+	
+	
+	public void m3() {
+		System.out.println("This is method m3");
+	}
+
+	
+	public void m4() {
+		System.out.println("This is method m4");
+	}
+}
